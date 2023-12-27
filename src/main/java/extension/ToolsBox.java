f5a5c490c8b1ca958c78d3eec4279d80295bf589
Ech0oo0 <darkops@163.com>
2023-12-27 12:28:39 +0800

    20231227

--- src/main/java/extension/ToolsBox.java
+++ src/main/java/extension/ToolsBox.java
@@ -19,7 +19,20 @@ public class ToolsBox implements BurpExtension {
         JPanel rootPanel = toolsBoxUI.rootPanel;
         api.userInterface().registerSuiteTab("T0ol5-BoX",rootPanel);
         // 打印加载成功信息
-        api.logging().logToOutput("~~Successfully loaded~~");
+        String loadSuccess = """
+                ========================================================================
+                  __  __            _____ ___           _                      \s
+                 |  \\/  |_ __      |  ___/ _ \\ _ __ ___(_) __ _ _ __   ___ _ __\s
+                 | |\\/| | '__|     | |_ | | | | '__/ _ \\ |/ _` | '_ \\ / _ \\ '__|
+                 | |  | | |     _  |  _|| |_| | | |  __/ | (_| | | | |  __/ |  \s
+                 |_|  |_|_|    (_) |_|   \\___/|_|  \\___|_|\\__, |_| |_|\\___|_|  \s
+                                                          |___/                \s
+                [ T0ol5-BoX v1.0 ] - [ LOAD SUCCESS! ]
+                - Author: Mr.F0reigner
+                - GitHub: https://github.com/Mr-F0reigner/BurpExtension-ToolsBox
+                ========================================================================
+                """;
+        api.logging().logToOutput(loadSuccess);
         api.userInterface().registerContextMenuItemsProvider(new ContextMenu());
     }
 }
