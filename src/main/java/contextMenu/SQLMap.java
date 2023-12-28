d36e6e553a0a58d13c1893108ef38a141309deb8
Ech0oo0 <darkops@163.com>
2023-12-28 17:44:54 +0800

    SQL Map

--- src/main/java/contextMenu/SQLMap.java
+++ src/main/java/contextMenu/SQLMap.java
@@ -5,17 +5,22 @@ import burp.api.montoya.core.ToolType;
 import burp.api.montoya.http.message.requests.HttpRequest;
 import burp.api.montoya.ui.contextmenu.ContextMenuEvent;
 import extension.ToolBox;
+import ui.ToolBoxUI;
 
 import javax.swing.*;
 import java.awt.*;
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class SQLMap {
     private MontoyaApi api = ToolBox.api;
     private ContextMenuEvent event;
     private List<Component> menuItemList;
-
+    private ArrayList<String> originalConfigData = ToolBoxUI.originalConfigData;
     public SQLMap(ContextMenuEvent event, List<Component> menuItemList) {
         this.event = event;
         this.menuItemList = menuItemList;
@@ -27,19 +32,44 @@ public class SQLMap {
             JMenuItem sqlMap = new JMenuItem("SQL Map");
             sqlMap.addActionListener(e -> {
                 try {
+                    // 使用空格分割 payload 字符串
+                    String[] payloadParts = originalConfigData.get(0).split(" ");
+
+                    // 获取用户桌面\SqlMapTemP.txt路径
+                    String filePath = "";
+                    Pattern pattern = Pattern.compile(".*\\.txt$");
+                    for (String part : payloadParts) {
+                        Matcher matcher = pattern.matcher(part);
+                        if (matcher.matches()) {
+                            filePath = part;
+                        }
+                    }
+
+                    // 判断文件是否存在
+                    File file = new File(filePath);
+                    if(!file.exists()) {
+                        file.createNewFile();
+                    }
+
                     // 保存请求包到文件中
                     String payload = String.valueOf(event.messageEditorRequestResponse().get().requestResponse().request());
-                    api.logging().logToOutput(String.valueOf(event.messageEditorRequestResponse().get().requestResponse().request()));
-                    BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("C:\\Users\\Xi_xi\\Desktop\\temp.txt"));
+                    BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
                     bufferedWriter.write(payload);
                     bufferedWriter.flush();
                     bufferedWriter.close();
-                    // 创建 ProcessBuilder 实例
-                    ProcessBuilder builder = new ProcessBuilder("cmd", "/c", "start", "cmd.exe", "/k", "C:\\Users\\Xi_xi\\AppData\\Local\\Programs\\Python\\Python37\\python.exe", "D:\\PT_Tools\\SQLMap\\sqlmap.py", "-r", "C:\\Users\\Xi_xi\\Desktop\\temp.txt", "--level", "1");
-                    builder.redirectErrorStream(true); // 将错误输出和标准输出合并
 
-                    // 启动进程
-                    Process process = builder.start();
+                    // 创建完整的命令数组，长度为 payloadParts.length + 5（因为有5个额外的命令部分）
+                    String[] command = new String[payloadParts.length + 5];
+                    command[0] = "cmd";
+                    command[1] = "/c";
+                    command[2] = "start";
+                    command[3] = "cmd";
+                    command[4] = "/k";
+                    System.arraycopy(payloadParts, 0, command, 5, payloadParts.length);
+
+                    // 创建并启动进程
+                    ProcessBuilder builder = new ProcessBuilder(command);
+                    builder.start();
                 } catch (IOException ioException) {
                     api.logging().logToOutput(ioException.getMessage());
                 }
--- src/main/java/ui/ToolBoxUI.java
+++ src/main/java/ui/ToolBoxUI.java
@@ -9,7 +9,10 @@ import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class ToolBoxUI {
     public MontoyaApi api = ToolBox.api;
@@ -24,20 +27,25 @@ public class ToolBoxUI {
     private JTextField domainTextField;
     private JPanel configPanel;
     private JScrollPane configScrollPane;
+    private JTable configTable;
+    private JButton saveBotton;
     private static DNSLog dnsLog = new DNSLog();
-    private DefaultTableModel dataModel = new DefaultTableModel();
+    private DefaultTableModel dnsLogModel = new DefaultTableModel();
+    private DefaultTableModel configModel = new DefaultTableModel();
+    public static ArrayList<String> originalConfigData = new ArrayList<>();
 
     public ToolBoxUI() {
         // 加载表单样式
-        setDataTableStyle();
-
+        setDnsLogStyle();
+        setConfigTableStyle();
+        saveOriginalData();
         // 获取域名点击事件
         getSubDomain.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 try {
-                    while (dataModel.getRowCount() > 0) {
-                        dataModel.removeRow(0);
+                    while (dnsLogModel.getRowCount() > 0) {
+                        dnsLogModel.removeRow(0);
                     }
                     domainTextField.setText(dnsLog.getDnslogDomain());
                 } catch (IOException ex) {
@@ -58,19 +66,19 @@ public class ToolBoxUI {
                         String data1 = record.getString(0); // 第一列
                         String data2 = record.getString(1); // 第二列
                         String data3 = record.getString(2); // 第三列
-                        // 检查数据是否已经存在于 dataModel 中
+                        // 检查数据是否已经存在于 dnsLogModel 中
                         boolean dataExists = false;
-                        for (int j = 0; j < dataModel.getRowCount(); j++) {
-                            if (dataModel.getValueAt(j, 0).equals(data1) &&
-                                    dataModel.getValueAt(j, 1).equals(data2) &&
-                                    dataModel.getValueAt(j, 2).equals(data3)) {
+                        for (int j = 0; j < dnsLogModel.getRowCount(); j++) {
+                            if (dnsLogModel.getValueAt(j, 0).equals(data1) &&
+                                    dnsLogModel.getValueAt(j, 1).equals(data2) &&
+                                    dnsLogModel.getValueAt(j, 2).equals(data3)) {
                                 dataExists = true;
                                 break;
                             }
                         }
-                        // 如果数据不存在，则添加到 dataModel
+                        // 如果数据不存在，则添加到 dnsLogModel
                         if (!dataExists) {
-                            dataModel.addRow(new Object[]{data1, data2, data3});
+                            dnsLogModel.addRow(new Object[]{data1, data2, data3});
                         }
                     }
                 } catch (IOException ex) {
@@ -78,17 +86,41 @@ public class ToolBoxUI {
                 }
             }
         });
-    }
 
-    private void setDataTableStyle() {
+
+        saveBotton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                for (int i = 0; i < configModel.getRowCount(); i++) {
+                    String currentData = configModel.getValueAt(i, 2).toString();
+                    String originalDataValue = originalConfigData.get(i);
+
+                    // 对比当前数据与原始数据
+                    if (!currentData.equals(originalDataValue)) {
+                        // 如果不同，根据需要进行处理
+                        // 例如，更新 originalData 和执行其他操作
+                        originalConfigData.set(i, currentData);
+                        // 这里可以添加其他需要的操作，比如更新数据库或执行其他任务
+                    }
+                }
+            }
+        });
+    }
+    private void saveOriginalData() {
+        originalConfigData.clear();
+        for (int i = 0; i < configModel.getRowCount(); i++) {
+            originalConfigData.add(configModel.getValueAt(i, 2).toString());
+        }
+    }
+    private void setDnsLogStyle() {
         // 设置域名文本框样式
         domainTextField.setBorder(null);    // 无边框
         domainTextField.setOpaque(false);   // 透明背景
         // 定义列名
         final Object[] columnNames = {"DNS Query Record", "IP Address", "Created Time"};
         // 使用 DefaultTableModel，解决JTable不显示问题。设置初始为空
-        dataModel = new DefaultTableModel(columnNames, 0);
-        dataTable.setModel(dataModel);
+        dnsLogModel = new DefaultTableModel(columnNames, 0);
+        dataTable.setModel(dnsLogModel);
         // 为每一列设置自定义渲染器，使数据居中显示
         DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
         centerRenderer.setHorizontalAlignment(JLabel.CENTER);
@@ -96,4 +128,49 @@ public class ToolBoxUI {
             dataTable.getColumnModel().getColumn(i).setCellRenderer(centerRenderer);
         }
     }
+
+    private void setConfigTableStyle() {
+        // 定义列名
+        final Object[] columnNames = {"#", "Key", "Value", "Comment"};
+        // 使用 DefaultTableModel，解决JTable不显示问题。设置初始为空
+        configModel = new DefaultTableModel(columnNames, 0);
+        configTable.setModel(configModel);
+
+        // 设置列的自动调整模式为AUTO_RESIZE_NEXT_COLUMN
+        configTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+
+        // 为 JScrollPane 添加组件大小变化监听器
+        configScrollPane.addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                adjustConfigColumnWidths();
+            }
+        });
+
+        // 初始调整列宽
+        adjustConfigColumnWidths();
+
+        // 为每一列设置自定义渲染器，使数据居中显示
+        DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
+        centerRenderer.setHorizontalAlignment(JLabel.CENTER);
+        for (int i = 0; i < configTable.getColumnCount(); i++) {
+            configTable.getColumnModel().getColumn(i).setCellRenderer(centerRenderer);
+        }
+        configModel.addRow(new Object[]{"1", "SQL Map", "C:\\Users\\Xi_xi\\AppData\\Local\\Programs\\Python\\Python37\\python.exe D:\\PT_Tools\\SQLMap\\sqlmap.py -r C:\\Users\\Xi_xi\\Desktop\\temp.txt --level 1", "Automatic execution of SQLMAP"});
+    }
+
+    private void adjustConfigColumnWidths() {
+        int totalWidth = configScrollPane.getViewport().getWidth();
+        int firstColumnWidth = 50;
+        int secondColumnWidth = 100;
+        configTable.getColumnModel().getColumn(0).setPreferredWidth(firstColumnWidth);
+        configTable.getColumnModel().getColumn(1).setPreferredWidth(secondColumnWidth);
+        int remainingWidth = totalWidth - firstColumnWidth - secondColumnWidth;
+        int otherColumnWidth = remainingWidth / 2; // 剩余两列平分宽度
+
+        for (int i = 2; i < configTable.getColumnCount(); i++) {
+            configTable.getColumnModel().getColumn(i).setPreferredWidth(otherColumnWidth);
+        }
+    }
+
 }
