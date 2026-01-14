2bc12edcffc8c52f9ded78b6edb1c10e9b6434ba
Mr-F0reigner <darkops.echo@outlook.com>
2026-01-14 10:40:44 +0800

    优化扫描逻辑及控件样式

--- src/main/java/main/ToolBox.java
+++ src/main/java/main/ToolBox.java
@@ -34,7 +34,7 @@ public class ToolBox implements BurpExtension {
                  | |  | | |     _  |  _|| |_| | | |  __/ | (_| | | | |  __/ |  \s
                  |_|  |_|_|    (_) |_|   \\___/|_|  \\___|_|\\__, |_| |_|\\___|_|  \s
                                                           |___/                \s
-                [ T0o1-Bo* v1.8 ] - [ LOAD SUCCESS! ]
+                [ T0o1-Bo* v1.9 ] - [ LOAD SUCCESS! ]
                 - Author: Mr.F0reigner
                 - GitHub: https://github.com/Mr-F0reigner/Burp-ToolBox
                 ========================================================================
